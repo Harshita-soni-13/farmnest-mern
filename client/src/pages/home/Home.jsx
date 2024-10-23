97c566a7b3fa2341591cabf237b01443c1e1fe17
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-10-23 07:39:19 +0530

    Currection of importing images

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ import { Blog } from "./latestBlog/Blog"
 import { Card } from "./cards/Card"
 import { FarmFields } from "./farm-fields/FarmFields"
 import { Header } from "./header/Header"
+import OurServices from "./ourServices/OurServices"
+import OurCustomerSay from "./ourCustomers/OurCustomerSay"
+import WhatWeDo from "./whatwedo/WhatWeDo"
 
 export const Home = () => {
   return (
@@ -12,6 +15,9 @@ export const Home = () => {
    <Card/>
    <AboutUs/>
    <FarmFields/>
+   <OurServices/>
+   <OurCustomerSay />
+   <WhatWeDo />
    <Blog/>
    <Footer/>
    </div>
